3b2579d57c94cf52456095251a06b3859d1b8efe
Nate Smith <158646537+natesmith21@users.noreply.github.com>
2026-02-13 10:41:15 -0800

    updated about me

--- src/componenets/About.jsx
+++ src/componenets/About.jsx
@@ -11,13 +11,21 @@ const AboutMe = () => {
             I live in Spokane, WA with my wife and dog. We moved here from Charlotte, North Carolina in 2022 for my wife, Karla, to pursue her JD at Gonzaga University. I completed a BA in Communication in 2017 and an MBA in 2020, both from Queens University of Charlotte. I'm currently searching for my next career.
         </p>
         <p>
-          	You will see in my resume that most of my previous work experience is at Queens where I worked in the Admissions Office, primarily on the Operations Team where we used a robust Admissions CRM, Slate. Our team was integral to enrolling students. I worked to ensure that our Admissions staff could smoothly collect and review applications, find and communicate with students in a timely manner. I also collaborated with the marketing team to deliver accurate, timely mass communications, as well created and maintained interactive 'portal' webpages for students to use to track their application status and receive updates. Lastly, I created and maintained our application funnel data, working with our Vice President to design clear and accurate reports to track our progress.
+            In my previous experience at Queens University I worked primarily in our Admissions CRM, Slate. I ensrued that we could collect information accuratly and effictively from studetns. I used data collected both in the application and collected though cookies, UTM tracking and the Financial Aid office to guide our admissions staff's communication efforts. I worked closely with IT, the Registrar's Office and Student Financial Services to ensure that data was integrated across each of our systems. Accurate and regular flow of information was vital for us to provide studetns with complete informaion to inform their enrollment decision. 
+        </p>
+
+        <p>
+            Accurate data is integral for decision making. I build and designed custom reports and queries used by our President and their VPs. Using Slate's report builder I created a year over year compairson report which compared to the day and could be run on demand. I built and maintained queries used by Instituational Reporting which they used to report to the Department of Education. 
+        </p>
+
+        <p>
+            Lasly, I collobrated with our Marketing department to design robust, personalized email and text campaigns with prospecitve students. I maintained queries of email recipients, built emails using HTMl/CSS and Liquid Markup. The marketing team and I worked together to design student facing portals, that I implemented HTML, CSS, Liquid Markup and JavaScript. 
         </p>
         <p>
-        	At the beginning of 2024 I began a Full Stack Software Engineering Certificate through Gonzaga University, hosted by Springboard. In June, 2024 I lost my job at Queens due to budget reductions. After working through several personal and professional challenges in the past year, I have completed my Certificate and am looking for my next opportunity. As I begin my search, I'm looking for roles to combine the skills I have, and help me continue to build and polish my technical ability. I believe I could be a great fit on a Software Development team, a Sales or Marketing Ops Team, or potentially as a project manager. 
+        	At the beginning of 2024 I began a Full Stack Software Engineering Certificate through Gonzaga University, hosted by Springboard which I completed in May of 2025. Currenly I work at the YMCA of the Inland Northwest where I serve as my branch's Facilites Director. Despite how it might seem at first, I see overlap in the skills I use to be successful in both positions. My ability to break down big, complex problems into smaller steps plus my research skills allow me to find accurate manuals and guides to assist me in producing the best work I can. 
         </p>
         <p>
-            I have worked on a wide variety of projects. My breadth of knowledge is my strength. Over the course of my short career I have been working to grow my technical skillset and striving to be an impactful member of every team I join.
+            I have worked on a wide variety of projects. My breadth of knowledge is my strength. Over the course of my short career I have been working to grow my technical skillset and striving to be an impactful member of every team I join. Wheather a system quesion, or a maintence question my answer is almost never "I don't know", but "I can find out". I strive to be the go-to person to get high quality work done in a timely manner.
 
         </p>
         </section>
