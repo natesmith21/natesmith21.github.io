e375ab78c863b741722bb506ad67f3bac0f874ad
Nate Smith <158646537+natesmith21@users.noreply.github.com>
2026-06-09 10:43:09 -0700

    update projects section

--- src/componenets/Projects.jsx
+++ src/componenets/Projects.jsx
@@ -13,24 +13,14 @@ const Projects = () => {
           <h2>My Projects</h2>
             <ul className="examples">
                 <li>
+                    <h4 className="projectTitle">Get-A-Job</h4>
+                    <p className="workLinks"><a href="https://get-a-job-fg15.onrender.com/" target="_blank"> Get-A Job</a></p>
+                    <p className="projectDesc"> A job search tool I’ve built as a Flask App. Users create job searches, save job posts to their profile to apply to. AI is integrated to provide user feedback on job match as well as optimize job search.</p>
+                </li>
+                <li>
                     <h4 className="projectTitle">Who's Hungry?</h4>
-                    <p className="workLinks"><a href="https://github.com/natesmith21/whos-hungry" target="_blank">GitHub</a> | <a href="https://whos-hungry-frontend.onrender.com/" target="_blank">Who's Hungry?</a></p>
-                    <p className="projectDesc"> Used React, Node, Express, PostgreSQL, and ReactStrap, to build a webpage to function as a recipe finder. </p>
-                </li>
-                <li>
-                    <h4 className="projectTitle">Jobly</h4>
-                    <p className="workLinks"><a href="https://github.com/natesmith21/react-jobly-frontend" target="_blank">GitHub</a> | <a href="https://react-jobly-frontend-ki3b.onrender.com/" target="_blank">Jobly</a></p>
-                    <p className="projectDesc"> I was provided a backend and filler data, then used React, Node, Express, PostgreSQL, and ReactStrap to build a job search platform. </p>
-                </li>
-                <li>
-                    <h4 className="projectTitle">Database DJ</h4>
-                    <p className="workLinks"><a href="https://github.com/hatchways-community/database-dj-672e1e3b746348dc9ac57f1a0675c68a" target="_blank">GitHub</a> | </p>
-                    <p className="projectDesc"> I created a simple Flask App using Django where users can create playlists. </p>
-                </li>
-                <li>
-                    <h4 className="projectTitle">Github: natesmith21</h4>
-                    <p className="workLinks"><a href="https://github.com/natesmith21" target="_blank">GitHub</a> | </p>
-                    <p className="projectDesc"> Feel free to check out any of my other work I'm done on my GitHub page. Currently, it's full of homework assignments from my certificate program. </p>
+                    <p className="workLinks"><a href="https://github.com/natesmith21/whos-hungry" target="_blank">GitHub</a></p>
+                    <p className="projectDesc"> My final project in my certificate. This simple app connects to the SpoonacularAPI to allow users to search for recipes or browse by cuisine type </p>
                 </li>
             </ul>
         </Container>
